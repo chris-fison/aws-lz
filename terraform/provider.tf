f25e9676f9ae9b6451ab22be3bf957574d5b02d4
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-19 14:30:39 +0000

    1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region  = "eu-west-2"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+ backend "s3" {
+    bucket         	   = "aws-lz-tfstate-s3"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-west-2"
+    encrypt        	   = true
+    dynamodb_table = "aws-lz-tfstate-cdb"
+  }
+}
