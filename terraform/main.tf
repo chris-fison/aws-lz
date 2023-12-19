927c8c89e371a6de354f82874c292716f9d586e0
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-19 22:55:30 +0000

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
   region = "eu-west-2"
 }
-
-terraform {
-  backend "s3" {
-    # encrypt = true
-    bucket = "aws-lz-tfstate-s3"
-    # dynamodb_table = "aws-lz-tfstate-cdb"
-    key    = "state/terraform-state.tf"
-    region = "eu-west-2"
-  }
-}
-
-
-# resource "aws_instance" "example" {
-#   ami           = "ami-0c55b159cbfafe1f0"
-#   instance_type = "t2.micro"
-# }
