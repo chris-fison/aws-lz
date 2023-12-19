33bee0d3e8e7c39799712813c4b3533170b26bce
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-19 14:36:30 +0000

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "demo" {
-  ami                 = var.ami
-  instance_type = var.type
+provider "aws" {
+  region = "eu-west-2"  # Specify your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-093cb9fb2d34920ad"  # Specify the AMI ID for your desired operating system
+  instance_type = "t2.micro"  # Specify the instance type
 
   tags = {
-    name = "My VM"
+    Name = "example-instance"
   }
 }
