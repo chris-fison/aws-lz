dd99f310f42b04d73beb8d97f57d8babc4acccf3
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-19 15:34:25 +0000

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,10 @@ terraform {
     key              	 = "state/terraform.tfstate"
     region         	   = "eu-west-2"
     encrypt        	   = true
-    dynamodb_table     = "aws-lz-tfstate-cdb"
+    # dynamodb_table     = "aws-lz-tfstate-cdb"
   }
 }
 
-# Define the EC2 instance
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Change this to the desired AMI ID
-  instance_type = "t2.micro"               # Change this to the desired instance type
 
-  tags = {
-    Name = "example-instance"
-  }
-}
+
+
