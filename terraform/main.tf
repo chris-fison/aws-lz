6e1ed50040144ecff15e8c1a42fb478674ee0d68
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-19 13:43:41 +0000

    s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-fisontech-tf-test-bucket"
 
   tags = {
     Name        = "My bucket"
