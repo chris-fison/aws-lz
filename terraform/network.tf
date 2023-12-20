535a34d4babc527cf38d5ac7e90d113c3964b499
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-20 17:04:52 +0000

    vpc4

--- terraform/network.tf
+++ terraform/network.tf
@@ -19,3 +19,16 @@ resource "aws_vpc" "fisontech_vpc3" {
     Name = "Fisontech-VPC3"
   }
 }
+
+
+# Adding new VPC
+resource "aws_vpc" "fisontech_vpc4" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Fisontech-VPC4"
+  }
+}
+
