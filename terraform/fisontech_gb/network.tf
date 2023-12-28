81c15629deaaee75065943b3edc3188c2663a462
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-28 13:50:18 +0000

    gb

--- terraform/fisontech_gb/network.tf
+++ terraform/fisontech_gb/network.tf
@@ -1,5 +1,5 @@
 # Adding new VPC
-resource "aws_vpc" "fisontech_pgb" {
+resource "aws_vpc" "fisontech_gb" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
