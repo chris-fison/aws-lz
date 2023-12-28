b0199071324f4e8f94ef9cf8da41cca107c247e8
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-28 15:55:07 +0000

    b

--- terraform/fisontech_ws/network.tf
+++ terraform/fisontech_ws/network.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "fisontech-ws"
   cidr = "10.10.0.0/16"
-  azs             = ["eu-west-1a", "eu-west-1b"]
+  azs             = ["eu-west-2a", "eu-west-2b"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
   public_subnets  = ["10.10.3.0/24", "10.10.4.0/24"]
   enable_nat_gateway = true
