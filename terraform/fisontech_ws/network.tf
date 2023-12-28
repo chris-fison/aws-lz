77a796808b31ede1c5329e3c22258c2a0733a5ea
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-28 17:06:58 +0000

    bpc

--- terraform/fisontech_ws/network.tf
+++ terraform/fisontech_ws/network.tf
@@ -16,13 +16,32 @@
 #   }
 # }
 
-# Adding new VPC
-resource "aws_vpc" "fisontech_ws_0" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# # Adding new VPC
+# resource "aws_vpc" "fisontech_ws_0" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+
+#   tags = {
+#     Name = "Fisontech-VPC-ws-0"
+#   }
+# }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name                 = "my-vpc-ws"
+  cidr                 = "10.0.0.0/16"
+  # azs                  = ["us-east-1a", "us-east-1b"]
+  # private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  # public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  # enable_nat_gateway   = true
+  # single_nat_gateway   = true
+  # enable_dns_support   = true
+  # enable_dns_hostnames = true
 
   tags = {
-    Name = "Fisontech-VPC-ws-0"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
