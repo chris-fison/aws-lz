93805a2d09f4dabb8c39f2eca305305517f3b190
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2023-12-28 17:11:18 +0000

    prov

--- terraform/fisontech_ws/main.tf
+++ terraform/fisontech_ws/main.tf
@@ -2,4 +2,14 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
 
--- terraform/fisontech_ws/network.tf
+++ terraform/fisontech_ws/network.tf
@@ -32,13 +32,13 @@ module "vpc" {
 
   name                 = "my-vpc-ws"
   cidr                 = "10.0.0.0/16"
-  # azs                  = ["us-east-1a", "us-east-1b"]
-  # private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  # public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
-  # enable_nat_gateway   = true
-  # single_nat_gateway   = true
-  # enable_dns_support   = true
-  # enable_dns_hostnames = true
+  azs                  = ["us-east-1a", "us-east-1b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform   = "true"
